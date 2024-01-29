764e2bd60ae50c12a7dd20d6757cab96f09c4361
nigelm48 <zcabnm0@ucl.ac.uk>
2024-01-29 13:00:34 +0000

    Added functions to tweet but still not tweeting

--- london_weather_bot.py
+++ london_weather_bot.py
@@ -1,3 +1,22 @@
+import tweepy
+import requests
+from datetime import datetime, timedelta
+import time
+
+# Twitter API credentials
+consumer_key = 'YOUR_CONSUMER_KEY'
+consumer_secret = 'YOUR_CONSUMER_SECRET'
+access_token = 'YOUR_ACCESS_TOKEN'
+access_token_secret = 'YOUR_ACCESS_TOKEN_SECRET'
+
+# OpenWeatherMap API key
+owm_api_key = 'YOUR_OPENWEATHERMAP_API_KEY'
+
+# Tweepy authentication
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
 def get_weather():
     # Replace 'YOUR_CITY' with 'London' or any other city you are interested in
     city = 'London'
@@ -8,9 +27,6 @@ def get_weather():
         response = requests.get(base_url, params=params)
         data = response.json()
 
-        # Print the entire response for debugging
-        print(data)
-
         # Extract relevant weather information
         temperature = data['main']['temp']
         description = data['weather'][0]['description']
@@ -19,3 +35,18 @@ def get_weather():
     except Exception as e:
         print(f"Error fetching weather data: {e}")
         return None
+
+def tweet_weather():
+    weather_info = get_weather()
+    if weather_info:
+        try:
+            # Post tweet
+            api.update_status(weather_info)
+            print(f"Tweeted: {weather_info}")
+        except tweepy.TweepError as te:
+            print(f"Error tweeting: {te}")
+
+# Tweet weather every hour
+while True:
+    tweet_weather()
+    time.sleep(3600)  # Sleep for 1 hour (3600 seconds)
